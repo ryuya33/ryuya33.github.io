6b62f032c5c861901024f2dec21d3df94690f7c1
ryuya33 <108787070+ryuya33@users.noreply.github.com>
2022-09-27 16:21:35 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -64,12 +64,21 @@
 
 // タブメニュー始め
   const menuItems = document.querySelectorAll('.menu li a');
+  const contents = document.querySelectorAll('.content');
 
-  menuItems.forEach(item => {
-    item.addEventListener('click', e => {
+  menuItems.forEach(clickedItem => {
+    clickedItem.addEventListener('click', e => {
       e.preventDefault();
 
-      item.classList.add('active');
+      menuItems.forEach(item => {
+        item.classList.remove('active');
+      })
+      clickedItem.classList.add('active');
+
+      contents.forEach(content => {
+        content.classList.remove('active');
+      })
+      document.getElementById(clickedItem.dataset.id).classList.add('active');
     });
   });
  
